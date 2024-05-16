76a34c5ab29ee1c9fa6d8e81cbe5439ef3d49994
Jacob Padley <jacob5180@hotmail.co.uk>
2024-05-16 22:52:54 +0100

    Add function for creating new TableViews

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/j3-n/gyrio/internal/components"
+)
 
 func main() {
-	fmt.Println("gyrio")
+	if err := ui.Init(); err != nil {
+		log.Fatalf("Error initialising termui: %v", err)
+	}
+	defer ui.Close()
+
+	table := components.NewTableView()
+	table.Title = "Customers"
+
+	grid := ui.NewGrid()
+	w, h := ui.TerminalDimensions()
+	grid.SetRect(0, 0, w, h)
+
+	grid.Set(ui.NewRow(1.0,
+		ui.NewCol(1.0, table),
+	),
+	)
+
+	ui.Render(grid)
+
+	events := ui.PollEvents()
+	for {
+		e := <-events
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "<Resize>":
+			resize := e.Payload.(ui.Resize)
+			grid.SetRect(0, 0, resize.Width, resize.Height)
+			ui.Clear()
+			ui.Render(grid)
+		}
+	}
 }
--- internal/components/table_view.go
+++ internal/components/table_view.go
@@ -4,10 +4,21 @@ import (
 	ui "github.com/gizak/termui/v3"
 )
 
+// TableView is a termui component for drawing SQL tables.
 type TableView struct {
 	ui.Block
+
+	// Columns is a slice of column titles
+	Columns []string
+}
+
+// NewTableView initialises and returns a new TableView widget.
+func NewTableView() *TableView {
+	return &TableView{
+		Block: *ui.NewBlock(),
+	}
 }
 
 func (t *TableView) Draw(buf *ui.Buffer) {
-
+	t.Block.Draw(buf)
 }
