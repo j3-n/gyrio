0d8dd08cd0912d632df1492c0fafcc4d3eb2c33d
Joseph Beck <joebeck03@icloud.com>
2024-05-22 13:43:02 +0100

    feat: add db connection cmds

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,25 +1,13 @@
 package cli
 
 import (
-	"fmt"
-	"io"
-
 	"github.com/urfave/cli/v2"
 )
 
-func init() {
-	cli.HelpPrinter = func(writer io.Writer, templ string, data interface{}) {
-		fmt.Fprintf(writer, "help\n")
-	}
-}
-
 func New() cli.App {
 	return cli.App{
-		Name:  "gyrio",
-		Usage: "view your databases in the terminal",
-		Action: func(*cli.Context) error {
-			fmt.Println("run gyrio help for more info")
-			return nil
-		},
+		Name:     "gyrio",
+		Usage:    "view your databases in the terminal",
+		Commands: cmds,
 	}
 }
